64cce9799f9eeea3e08ae886f3946c51c4e7c502
SomeshPadiyar <someshpadiyar@gmail.com>
2025-10-13 14:42:51 +0530

    added login with otp page

--- group-wallet/App.js
+++ group-wallet/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AuthFlow from './src/login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthFlow />
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <Text>hi somesh</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
